b28a6400a04dfdcf5619a68c74b1108652ded8c8
avijitneevcloud <avijit.ghosh@neevcloud.com>
2025-07-12 11:07:04 +0530

    code updated

--- src/assets/Portfolio_data.js
+++ src/assets/Portfolio_data.js
@@ -1,5 +1,6 @@
 import airbnb from '../assets/airbnb.webp'
 import spotify from '../assets/spotify.png'
+import Imagify from '../assets/imagify.jpg'
 
 const Portfolio_data = [
     {
@@ -11,28 +12,29 @@ const Portfolio_data = [
     {
         w_no:2,
         w_name:"Web Design",
-        w_img:spotify
+        w_img:Imagify,
+        w_link: 'https://imagifyprodev.netlify.app/'
     },
     {
         w_no:3,
         w_name:"Web Design",
-        w_img:airbnb
-    },
-    {
-        w_no:4,
-        w_name:"Web Design",
         w_img:spotify
     },
-    {
-        w_no:1,
-        w_name:"Web Design",
-        w_img:airbnb
-    },
-    {
-        w_no:1,
-        w_name:"Web Design",
-        w_img:airbnb
-    }
+    // {
+    //     w_no:4,
+    //     w_name:"Web Design",
+    //     w_img:spotify
+    // },
+    // {
+    //     w_no:1,
+    //     w_name:"Web Design",
+    //     w_img:airbnb
+    // },
+    // {
+    //     w_no:1,
+    //     w_name:"Web Design",
+    //     w_img:airbnb
+    // }
 ]
 
 export default Portfolio_data
